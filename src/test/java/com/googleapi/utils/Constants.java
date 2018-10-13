7f5460b919fd41d6cf50274185b66ebe8a17af49
Narottam Singh <narottamgla@gmail.com>
2018-10-13 11:54:44 +0530

    Updated google API Key

--- src/test/java/com/googleapi/utils/Constants.java
+++ src/test/java/com/googleapi/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.googleapi.utils;
 
 public class Constants {	
-	public static final String GOOGLE_API_KEY = "AIzaSyBWE5PudGYz76Udux93_wPX3xrcPTgltm0";
+	public static final String GOOGLE_API_KEY = "Google-API-Key";
 	public static final String GOOGLE_GEOCODE_ENDPOINTS = "https://maps.googleapis.com/maps/api/geocode/json";	
 }
